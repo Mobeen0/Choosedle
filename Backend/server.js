2fd45a96d9142874816f6af3ee8fdd3ed65fe59e
Muhammad Mobeen <muhammad.mobeen100@gmail.com>
2024-06-20 10:15:06 +0500

    starting backend work

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 
+const axios = require('axios');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('./', (req,res)=>{console.log('it is what it is')})
+app.get('/', (req,res)=>{console.log('it is what it is')})
+
+// axios.get('https://wordsapiv1.p.mashape.com/words/example').then((response)=>{
+//     console.log('sucessfully recieved data')
+//     console.log(response);
+// })
 
 
 app.listen(5000,()=>{
