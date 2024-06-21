e97a0ca8e9613309a2c8f74ce2fc6516f6199cde
Muhammad Mobeen <muhammad.mobeen100@gmail.com>
2024-06-21 18:44:58 +0500

    working on api integration

--- Backend/server.js
+++ Backend/server.js
@@ -10,10 +10,15 @@ app.use(express.json());
 
 app.get('/', (req,res)=>{console.log('it is what it is')})
 
-// axios.get('https://wordsapiv1.p.mashape.com/words/example').then((response)=>{
-//     console.log('sucessfully recieved data')
-//     console.log(response);
-// })
+axios.get('https://random-word-api.herokuapp.com/word', {
+    params: {
+      number: 1,
+      length: 5
+    }
+  }).then((response)=>{
+    console.log('sucessfully recieved data')
+})
+
 
 
 app.listen(5000,()=>{
