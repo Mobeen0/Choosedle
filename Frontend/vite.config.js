103f78f48ba58875a433c48d0a4d75e795a24ffd
Muhammad Mobeen <muhammad.mobeen100@gmail.com>
2024-07-12 23:16:43 +0500

    added deployment instructions

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-})
+  build: {
+    rollupOptions: {
+      external: ['mobx-react-lite'],
+    },
+  },
+});
