1fc41c06d409ce0a1dd910b5ca3a4b0d01e7d2c3
Muhammad Mobeen <muhammad.mobeen100@gmail.com>
2024-07-12 23:44:30 +0500

    fixed the vite config file

--- Frontend/src/components/ScreenKeyboard.jsx
+++ Frontend/src/components/ScreenKeyboard.jsx
@@ -1,4 +1,4 @@
-import { observer } from "mobx-react-lite"
+import { observer } from "mobx-react"
 
 export default observer(function ScreenKeyboard({ store }) {
   const qwerty = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['mobx-react-lite'],
-    },
-  },
+
 });
